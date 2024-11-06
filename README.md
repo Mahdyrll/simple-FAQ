49d8f79e2403d2e06e40dda1ba71aa7746a99cfd
Mahdyar <mahdiar871@gmail.com>
2024-11-06 17:22:19 +0330

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Screenshots
+
+![App Screenshot](./screencapture-file-Users-Mahdyar-Desktop-M-front-end-FAQ-index-html-2024-11-06-17_21_17.png)
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    fontFamily: {
+      "medium": "medium",
+      "regular": "regular",
+      "semibold": "semibold"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
